61e42a06b1b3989a29c204d63311bd3799df8f4d
alvrok <dasilvaronald12@gmail.com>
2025-06-01 22:37:47 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import gradio as gr
 from ultralytics import YOLO
-import cv2
+import os
 
 model = YOLO("best.pt")
 
@@ -10,4 +10,8 @@ def detectar(imagen):
     return imagen_anotada
 
 demo = gr.Interface(fn=detectar, inputs="image", outputs="image", title="Detección de Daños en Carreteras")
-demo.launch()
+
+port = int(os.environ.get("PORT", 7860))  # Render asigna el puerto en la variable PORT
+
+demo.launch(server_name="0.0.0.0", server_port=port)
+
